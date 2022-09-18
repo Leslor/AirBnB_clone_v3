f1a4ee141ab4d10d9a411858f43da8276ab04fd2
LesLor <leslie.paz.am@gmail.com>
2022-09-18 05:51:04 +0000

    Feat: Task-5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 """This module implement a rule that
 returns the status of the application"""
 
@@ -12,3 +20,17 @@ def status():
         "status": "OK"
     }
     return jsonify(state)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """endpoint that retrieves the number of each objects by type"""
+    total_classes = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+    return jsonify(total_classes)
