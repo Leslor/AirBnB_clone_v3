29276cfcffb80c71ad520e37c7bfd8dcda725b5b
Christian Varas <christianvaras.2020@gmail.com>
2022-09-18 16:11:38 -0500

    Fix pycodestyle

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -67,6 +67,7 @@ test_db_storage.py'])
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
+
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """ Test storage"""
@@ -86,6 +87,7 @@ test_db_storage.py'])
         new_count_class = models.storage.count('State')
         self.assertEqual(count + 1, new_count)
         self.assertEqual(count_class + 1, new_count_class)
+
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
